c76b82ffee862b82864773893c15da9ac0de4220
Fotis Gimian <fgimiansoftware@gmail.com>
2017-01-30 15:45:59 +1100

    Made all variables in software configs more consistent

--- build.py
+++ build.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import os
-
 from elite.decorators import elite_main
 
 
@@ -32,41 +30,32 @@ def main(elite, config, printer):
     restart_dock = False
 
     for group, software_items in config.software.items():
+        # Print the software group heading
         printer.heading(group)
 
         for software in software_items:
+            # Avoid modifying the original config
             software = software.copy()
 
-            if 'appstore' in software and 'name' not in software:
-                software['name'] = software['appstore']
-
-            if 'name' not in software:
+            # Obtain and print the software name
+            try:
+                name = software.pop('name')
+                printer.info(name)
+            except KeyError:
                 elite.fail(message=f'the software item {software} has no name')
 
-            name = software.pop('name')
+            # Obtain the app name (as found under /Applications)
+            app = software.pop('app', name)
+
+            # Taps
             tap = software.pop('tap', None)
-            appstore = software.pop('appstore', None)
-            cask = software.pop('cask', None)
-            brew = software.pop('brew', None)
-            pip = software.pop('pip', None)
-            npm = software.pop('npm', None)
-            files = software.pop('files', None)
-            download = software.pop('download', None)
-            git = software.pop('git', None)
-            rsync = software.pop('rsync', None)
-            symlinks = software.pop('symlinks', None)
-            defaults = software.pop('defaults', None)
-            json = software.pop('json', None)
-            handlers = software.pop('handlers', None)
-            login_item = software.pop('login_item', False)
-
-            printer.info(name)
-
             if tap:
                 taps = tap if isinstance(tap, list) else [tap]
                 for tap in taps:
                     elite.tap(name=tap)
 
+            # App store apps
+            appstore = software.pop('appstore', None)
             if appstore:
                 app_file = elite.file_info(
                     path=f'/Applications/{appstore}.app/Contents/_MASReceipt/receipt'
@@ -77,38 +66,39 @@ def main(elite, config, printer):
                         ignore_failed=True
                     )
 
+            # Cask packages
+            cask = software.pop('cask', None)
             if cask:
                 casks = cask if isinstance(cask, list) else [cask]
                 for cask in casks:
                     elite.cask(name=cask, env=env)
 
+            # Brew packages
+            brew = software.pop('brew', None)
             if brew:
                 brews = brew if isinstance(brew, list) else [brew]
                 for brew in brews:
                     elite.brew(name=brew, state='latest')
 
+            # Python pip packages
+            pip = software.pop('pip', None)
             if pip:
                 pips = pip if isinstance(pip, list) else [pip]
                 for pip in pips:
                     elite.pip(name=pip, state='latest', executable='pip3')
 
+            # Node.js npm packages
+            npm = software.pop('npm', None)
             if npm:
                 npms = npm if isinstance(npm, list) else [npm]
                 for npm in npms:
                     elite.npm(name=npm, mode='global')
 
-            if files:
+            # Files
+            file = software.pop('file', None)
+            if file:
+                files = file if isinstance(file, list) else [file]
                 for file in files:
-                    if (
-                        file.get('state', 'file') == 'file' and
-                        file['path'] != '/' and file['path'] != '~'
-                    ):
-                        elite.file(
-                            path=os.path.dirname(file['path']),
-                            state='directory',
-                            sudo=file.get('sudo', False)
-                        )
-
                     elite.file(
                         path=file['path'],
                         source=file.get('source'),
@@ -120,46 +110,53 @@ def main(elite, config, printer):
                         sudo=file.get('sudo', False)
                     )
 
-            if download:
-                downloads = download if isinstance(download, list) else [download]
-                for download in downloads:
-                    elite.download(path=download['path'], url=download['url'])
-
-            if git:
-                gits = git if isinstance(git, list) else [git]
-                for git in gits:
-                    elite.git(path=git['path'], repo=git['repo'])
-
+            # Folder to syncronize
+            rsync = software.pop('rsync', None)
             if rsync:
                 rsyncs = rsync if isinstance(rsync, list) else [rsync]
                 for rsync in rsyncs:
                     elite.rsync(path=rsync['path'], source=rsync['source'])
 
-            if symlinks:
-                for symlink in symlinks:
-                    if symlink['path'] != '/' and symlink['path'] != '~':
-                        elite.file(
-                            path=os.path.dirname(symlink['path']),
-                            state='directory',
-                            sudo=symlink.get('sudo', False)
-                        )
+            # Dowlnoads
+            download = software.pop('download', None)
+            if download:
+                downloads = download if isinstance(download, list) else [download]
+                for download in downloads:
+                    elite.download(path=download['path'], url=download['url'])
 
+            # Git repositories
+            git = software.pop('git', None)
+            if git:
+                gits = git if isinstance(git, list) else [git]
+                for git in gits:
+                    elite.git(path=git['path'], repo=git['repo'])
+
+            # Symbolic links
+            symlink = software.pop('symlink', None)
+            if symlink:
+                symlinks = symlink if isinstance(symlink, list) else [symlink]
+
+                for symlink in symlinks:
                     symlink_health = elite.file_info(path=symlink['path'])
                     if symlink_health.exists and symlink_health.file_type != 'symlink':
                         elite.file(path=symlink['path'], state='absent')
-
                     elite.file(path=symlink['path'], source=symlink['source'], state='symlink')
 
-            if defaults:
-                for default in defaults:
+            # plist settings
+            plist = software.pop('plist', None)
+            if plist:
+                plists = plist if isinstance(plist, list) else [plist]
+                for plist in plists:
                     elite.plist(
-                        domain=default.get('domain'),
-                        values=default.get('values'),
-                        path=default.get('path'),
-                        container=default.get('container'),
-                        sudo=default.get('sudo', False)
+                        domain=plist.get('domain'),
+                        values=plist.get('values'),
+                        path=plist.get('path'),
+                        container=plist.get('container'),
+                        sudo=plist.get('sudo', False)
                     )
 
+            # JSON settings
+            json = software.pop('json', None)
             if json:
                 jsons = json if isinstance(json, list) else [json]
                 for json in jsons:
@@ -168,19 +165,12 @@ def main(elite, config, printer):
                         values=json.get('values')
                     )
 
+            # Application SpecificActions
             if name == 'Finder':
                 layout = software.pop('favourites')
+
                 elite.favourites(layout=layout)
-            elif name == 'Dock':
-                app_layout = software.pop('apps')
-                other_layout = software.pop('other')
-                dock = elite.dock(app_layout=app_layout, other_layout=other_layout)
-                restart_dock = restart_dock or dock.changed
-            elif name == 'Launchpad':
-                widget_layout = software.pop('widgets')
-                app_layout = software.pop('apps')
-                launchpad = elite.launchpad(widget_layout=widget_layout, app_layout=app_layout)
-                restart_dock = restart_dock or launchpad.changed
+
             elif name == 'Docker':
                 docker_create = elite.run(
                     command='docker-machine create --driver virtualbox default',
@@ -188,12 +178,16 @@ def main(elite, config, printer):
                 )
                 if docker_create.changed:
                     elite.run(command='docker-machine stop default')
+
             elif name == 'VIM':
                 vimrc_path = software.pop('vimrc_path')
+
                 elite.brew(name='vim', options='--with-lua', state='latest')
+
                 vimrc = elite.file(path='~', source=vimrc_path)
                 if vimrc.changed:
                     elite.run(command='vim +PluginInstall +qall')
+
             elif name == 'Spotify':
                 global_settings = software.pop('global_settings')
                 user_settings = software.pop('user_settings')
@@ -206,24 +200,43 @@ def main(elite, config, printer):
                     elite.spotify_pref(pref=pref, value=value, username=username)
 
                 elite.login_item(path='/Applications/Spotify.app', state='absent')
+
             elif name == 'Native Instruments Kontakt 5':
                 library_order = software.pop('library_order')
+
                 for index, library in enumerate(library_order):
                     elite.plist(
                         domain=f'com.native-instruments.{library}',
                         values={'UserListIndex': index}
                     )
 
-            if handlers:
+            elif name == 'Dock':
+                app_layout = software.pop('apps')
+                other_layout = software.pop('other')
+
+                dock = elite.dock(app_layout=app_layout, other_layout=other_layout)
+                restart_dock = restart_dock or dock.changed
+
+            elif name == 'Launchpad':
+                widget_layout = software.pop('widgets')
+                app_layout = software.pop('apps')
+
+                launchpad = elite.launchpad(widget_layout=widget_layout, app_layout=app_layout)
+                restart_dock = restart_dock or launchpad.changed
+
+            # File handlers
+            handler = software.pop('handler', None)
+            if handler:
+                handlers = handler if isinstance(handler, list) else [handler]
                 for handler in handlers:
-                    elite.handler(
-                        path=f'/Applications/{name}.app',
-                        content_type=handler
-                    )
+                    elite.handler(path=f'/Applications/{app}.app', content_type=handler)
 
+            # Login items
+            login_item = software.pop('login_item', False)
             if login_item:
-                elite.login_item(path=f'/Applications/{name}.app')
+                elite.login_item(path=f'/Applications/{app}.app')
 
+            # Verify that no extra keys remain after processing a piece of  software
             if software:
                 elite.fail(
                     message=(
