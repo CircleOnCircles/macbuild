83f45b7f0fef7564771213419ddb1df7b132254b
Fotis Gimian <fgimiansoftware@gmail.com>
2018-09-29 16:05:07 +1000

    Enabled login items now that the related action has been corrected to work in Mojave

--- macbuild.py
+++ macbuild.py
@@ -186,11 +186,10 @@ def main(elite, printer):
 
             # Login items
             login_item = software.pop('login_item', None)
-            # TODO: Determine what we will do about login items in Mojave
-            # if login_item is not None:
-            #     elite.login_item(
-            #         path=f'/Applications/{app}.app', state='present' if login_item else 'absent'
-            #     )
+            if login_item is not None:
+                elite.login_item(
+                    path=f'/Applications/{app}.app', state='present' if login_item else 'absent'
+                )
 
             # Verify that no extra keys remain after processing a piece of software
             if software:
